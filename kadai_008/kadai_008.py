cecf3149fff64af9ea0bab903465a88a1c5989b9
takahiro-kani <imasokari_sg@yahoo.co.jp>
2024-01-14 12:24:52 +0900

    kadai_008追加

---
+++ kadai_008/kadai_008.py
@@ -0,0 +1,10 @@
+import random
+var = random.randint(0,100)
+if var%3 == 0 and var%5 ==0:
+  print("FizzBuzz")
+elif var%3 == 0:
+  print("Fizz")
+elif var%5 == 0:
+  print("Buzz")
+else:
+  print(var)
