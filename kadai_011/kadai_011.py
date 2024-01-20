de841b836dc6a88433f8d999369770c3318b799c
takahiro-kani <imasokari_sg@yahoo.co.jp>
2024-01-20 21:17:38 +0900

    kadai_011修正

--- kadai_011/kadai_011.py
+++ kadai_011/kadai_011.py
@@ -1,20 +1,8 @@
-# for
-
 my_list = ["水","金","地","火","木","土","天","海","冥"]
 
-for num in range(0,2):
-  for i in range(0,9):
-    print(my_list[i])
-
-my_list = ["水","金","地","火","木","土","天","海","冥"]
-
-# while
-
-num = 0
-while num < 2:
-  index = 0
-  while index < 9:
-    print(my_list[index])
-    index = index + 1
-  
-  num = num + 1
+for i in range(0,9):
+  print(my_list[i])
+index = 0
+while index < 9:
+  print(my_list[index])
+  index = index + 1
