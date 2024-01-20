16782ae2651abd3d74a9a00635b6dcd79919803b
takahiro-kani <imasokari_sg@yahoo.co.jp>
2024-01-20 22:02:13 +0900

    kadai_013/修正

--- kadai_013/kadai_013.py
+++ kadai_013/kadai_013.py
@@ -1,4 +1,5 @@
-def purcahed_cost(price:int, tax:int):
-  total = price + price * tax/100
-  print(total)
-purcahed_cost(100, 10)
+def calculate_total(price:int , tax:int):
+  total_price = price * (1 + tax/100 )
+  return total_price
+result = calculate_total(1000 , 10)
+print(result)
